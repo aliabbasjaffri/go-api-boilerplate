72a620a132016c7c4b1db7e48a038833c0cacd4b
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2020-12-26 12:23:36 +0100

    WIP: Added address model

--- model/user.go
+++ model/user.go
@@ -1,7 +1,12 @@
 package model
 
 type User struct {
-	Name  string `json:"name"`
-	Age   int    `json:"age"`
-	Email string `json:"email"`
+	Name  string        `json:"name"`
+	Age   int           `json:"age"`
+	Email string        `json:"email"`
+	//Address * Address   `json:"address"`
 }
+
+//type Address struct {
+//
+//}
