8d0b458b2e9164f0842a2dff447560a1c1a731df
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2020-12-30 12:23:06 +0100

    Beauti-fy-ing code

--- app.go
+++ app.go
@@ -4,18 +4,12 @@ import (
 	"fmt"
 	"github.com/aliabbasjaffri/go-api-boilerplate/controller"
 	"github.com/gorilla/mux"
+	"log"
 	"net/http"
 )
 
 func main () {
-	//MONGO_DB_URI := "localhost"
-	//MONGO_DB_PORT := 27017
-	//fmt.Println("hello world")
-	fmt.Println("Starting the application...")
-
-	//ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	//clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-	//client, _ = mongo.Connect(ctx, clientOptions)
+	fmt.Println("Starting the API")
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", controller.GetAllUsers).Methods("GET")
@@ -23,5 +17,5 @@ func main () {
 	router.HandleFunc("/adduser", controller.CreateUser).Methods("POST")
 	router.HandleFunc("/updateuser", controller.UpdateUser).Methods("PUT")
 	router.HandleFunc("/deleteuser", controller.DeleteUser).Methods("DELETE")
-	http.ListenAndServe(":9999", router)
+	log.Fatal(http.ListenAndServe(":9999", router))
 }
